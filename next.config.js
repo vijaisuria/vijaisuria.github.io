efc53f393975fbaf148d66f3ea89c3bb92e88d5f
VIJAI SURIA <98590895+vijaisuria@users.noreply.github.com>
2023-07-04 00:08:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
   eslint: {
     ignoreDuringBuilds: true,
 },
