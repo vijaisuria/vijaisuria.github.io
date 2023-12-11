1493aa8346721a223c14efea3f03ae3191921e50
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2023-12-12 00:26:47 +0530

    Added google site verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,34 @@
-import '@/common/styles/global.css';
+import "@/common/styles/global.css";
 
-import localFont from '@next/font/local';
-import { Analytics } from '@vercel/analytics/react';
-import { MotionConfig } from 'framer-motion';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import localFont from "@next/font/local";
+import { Analytics } from "@vercel/analytics/react";
+import { MotionConfig } from "framer-motion";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-import { ThemeProvider } from '@/modules/themeContext';
-import { MouseVariantProvider } from '@/modules/customMouse';
+import { ThemeProvider } from "@/modules/themeContext";
+import { MouseVariantProvider } from "@/modules/customMouse";
 
 const ArticulatCF = localFont({
-  src: '../common/fonts/ArticulatCF-Bold.otf',
+  src: "../common/fonts/ArticulatCF-Bold.otf",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Vijai Suria | Software Developer</title>
+        <title>Vijai Suria | SDE</title>
         <meta
           name="description"
           content="Portoflio site of Software Developer Vijai Suria."
         />
         <meta
           name="keywords"
-          content="developer, vijai, suria, software"
+          content="vijaisuria, vijai suria, vijai, suria, sde, anna, university"
+        />
+        <meta
+          name="google-site-verification"
+          content="fZIGN9tZ-qZQT9oPQRkI0K-gzZc4XkbzeJmMHUH0hoc"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -32,7 +36,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <MotionConfig
           transition={{ ease: [0.6, 0.01, -0.05, 0.9], duration: 1 }}
         >
-          <ThemeProvider>  
+          <ThemeProvider>
             <MouseVariantProvider>
               <Component {...pageProps} />
             </MouseVariantProvider>
