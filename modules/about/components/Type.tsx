8a3d2acc3e4328fd41f70c90b0bcddf8c3b47544
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-05-11 16:48:06 +0530

    Updated type pages

--- modules/about/components/Type.tsx
+++ modules/about/components/Type.tsx
@@ -8,7 +8,7 @@ function Type() {
     "creates <br> interactive  <span class='text-gradient'>web</span> application.",
     "trains <br> cutting-edge <span class='text-gradient'>ML</span> models.",
     "builds <br> robust <span class='text-gradient'>API</span> services.",
-    "contibutes <br> to <span class='text-gradient'>Open Source</span> Projects.",
+    "contributes <br> to <span class='text-gradient'>Open Source</span> Projects.",
   ];
 
   return (
