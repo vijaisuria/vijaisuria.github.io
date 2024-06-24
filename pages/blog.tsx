8f12abd57c292c780b45376def3cabccabb9a69f
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-06-25 03:17:09 +0530

    Added blog link

---
+++ pages/blog.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+export default function ResumePage() {
+  useEffect(() => {
+    window.location.href =
+      "https://www.linkedin.com/pulse/revolutionizing-payments-ai-cognitopays-smart-easy-p2p-vijai-suria-3sghc";
+  }, []);
+  return (
+    <>
+      <h2>
+        Redirecting to
+        <a href="https://www.linkedin.com/pulse/revolutionizing-payments-ai-cognitopays-smart-easy-p2p-vijai-suria-3sghc">
+          https://www.linkedin.com/pulse/revolutionizing-payments-ai-cognitopays-smart-easy-p2p-vijai-suria-3sghc
+        </a>
+      </h2>
+    </>
+  );
+}
